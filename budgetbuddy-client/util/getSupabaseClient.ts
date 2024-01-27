13ce659f9fe8371b54d8234338098dbe48140fb6
Alec <alectan@hotmail.com>
2024-01-27 20:18:13 +1100

    feat(BGTB-12): Added workflow to build and deploy to staging on push to dev

--- budgetbuddy-client/util/getSupabaseClient.ts
+++ budgetbuddy-client/util/getSupabaseClient.ts
@@ -8,6 +8,9 @@ import { NextResponse, type NextRequest } from "next/server";
 import { cookies } from 'next/headers';
 import type { Database } from "@/types/supabase";
 
+const DEFAULT_SUPABASE_URL = "http://127.0.0.1:54321";
+const DEFAULT_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
+
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
