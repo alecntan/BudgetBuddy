a4cf5f3c91a10f8a54876de1d643227c2b31c1de
Alec <alectan@hotmail.com>
2024-01-27 12:31:56 +1100

    feat(BGTB-12) : Created simple not-found page'

---
+++ budgetbuddy-client/app/not-found.tsx
@@ -0,0 +1,9 @@
+import { Heading, Center } from "@chakra-ui/react";
+
+export default function NotFound() {
+    return (
+        <Center width={'100vw'} height={'100vh'}>
+            <Heading>Page Not Found</Heading>
+        </Center>
+    );
+}
